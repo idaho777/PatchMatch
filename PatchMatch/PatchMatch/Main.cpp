ffd3dedba9c478ffde25da95d2cfac7efbf701ab
Joonho <idaho777@gmail.com>
2020-02-10 02:15:14 -0500

    asdiof

--- PatchMatch/PatchMatch/Main.cpp
+++ PatchMatch/PatchMatch/Main.cpp
@@ -16,12 +16,51 @@ int main(int argc, char** argv)
 
     Mat image;
     image = imread(argv[1], IMREAD_COLOR); // Read the file
-    
+
 
     MatIterator_<Vec3b> it;
     for (it = image.begin<Vec3b>(); it != image.end<Vec3b>(); ++it) {
-        cout << (*it) << endl;
+        //cout << (*it) << endl;
     }
+    /*
+        Goal: Filling in hole
+        Stretch Goal: Filling in hole with constraints
+
+        Get Image to Fix
+        Color Image in another program
+        Read image in
+
+        Section 3:
+            - Nearest Neighbor Field (NNF) f: A -> R2 defined over all patch coordinates(patch centers) in image A.
+                In this case, f(a) = b - a, or a vector indicating the coordinate offset.
+                We need to create an offset array and then use this offset array to color our image.
+
+            - Initialization:
+                Initialize all offsets as zero except for those colored: be random vector
+
+            -Iterations:
+                Improving the mapping.  Alternate between propogation and random search.
+                - Propogation:
+                    for (x,y) colored, check left and top.  argmin(Distance of {left, center, top})
+                    Examine iterations in reverse for even iteration numbers.
+                - Random Search:
+                    Find a random vector ui = v0 + w a^i R, where v0 is f(x,y), w = maximum image dimension, a is ratio of window sizes, R is unit window.
+                Do this iteraition 5 times.
+
+        With the mapping, create new image with mappings.
+
+        
+        Iterative Process:
+            - Propagation:
+
+
+        Get Image
+        Color Image
+
+    
+    
+    
+    */
 
 
     if (!image.data) // Check for invalid input
